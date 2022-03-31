a3cef8c754354b8556f413e91485c5310cdd4692
Thuy Nguyen <ttruong06@gmail.com>
2022-03-31 17:43:12 -0400

    forecast dates

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,21 @@ var cityForm = document.querySelector("#city-form");
 var cityNameInput = document.querySelector("#city-name");       //form input
 var cityContainer = document.querySelector("#city-container");
 var dataSearchTerm = document.querySelector("#data-search-term"); //City: results
+var forecast1 = document.querySelector("#card-1");
+var forecast2 = document.querySelector("#card-2");
+var forecast3 = document.querySelector("#card-3");
+var forecast4 = document.querySelector("#card-4");
+var forecast5 = document.querySelector("#card-5");
+var forecastDate1 = document.querySelector("#forecastDate1");
+var forecastDate2 = document.querySelector("#forecastDate2");
+var forecastDate3 = document.querySelector("#forecastDate3");
+var forecastDate4 = document.querySelector("#forecastDate4");
+var forecastDate5 = document.querySelector("#forecastDate5");
 
 let city = document.getElementById('city-name').value;
 
+
+//Get date only for javaScript
 const date = new Date();
 const month = date.getMonth()+1;
 const day = date.getDate();
@@ -40,12 +52,32 @@ var getCityData = function(city) {
                      dataSearchTerm.innerHTML = `<h2 class="city-title">${name + fullYear}</h2>`;
   
                 //make a 2nd url request for the weather data
-                var apiUrl2 = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly,daily,alerts&appid=${api_key}`;  
+                var apiUrl2 = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly,alerts&appid=${api_key}`;  
                 
                 fetch(apiUrl2).then(function(response){
                     response.json().then(function(data) {
                         console.log(data);
                         displayData(data);  
+
+                        const { dt } = data.daily[1];
+                        const { temp, wind_speed, humidity } = data.daily[1]; 
+                        const { icon } = data.current.weather[0]; 
+                        
+                        const milliseconds = dt * 1000;
+                        const dateObject = new Date(milliseconds);
+                        const humanDate = dateObject.toLocaleString("en-US", {timeZoneName: "short"});
+                        const splits1 = humanDate.split(", ")[0];
+                        console.log(splits1);
+                        forecastDate1.innerHTML = `<div class="F-date">${splits1}</div>`
+
+                        forecast1.innerHTML =
+                            `<div class="forecast">
+                                <div class="w-icon"><img src="http://openweathermap.org/img/w/${icon}.png"></div>
+                                <div>Temp:   ${temp.day}°F</div>
+                                <div>Wind:   ${wind_speed} MPH</div>
+                                <div>Humidity:   ${humidity}%</div> 
+                            </div>`
+
                     }); 
                 });
             })
@@ -61,52 +93,26 @@ var displayData = function(data) {
 
     const { temp, wind_speed, humidity, uvi } = data.current;   
     const { icon } = data.current.weather[0]; 
-    
 
     cityContainer.innerHTML = 
         `<div class="weatherInfo">
+            <div class="w-icon"><img src="http://openweathermap.org/img/w/${icon}.png"></div>
+            <div class="weatherInfo">
             <div>Temp:   ${temp}°F</div>
-        </div>
-        <div class="weatherInfo">
             <div>Wind:   ${wind_speed} MPH</div>
-        </div>
-        <div class="weatherInfo">
             <div>Humidity:   ${humidity}%</div>
-        </div>
-        <div class="weatherInfo">
             <div>UV Index:   ${uvi}</div>
         </div>`;
+        
 }
+
+
+
 
 // call the function
 cityForm.addEventListener("submit", formSubmitHandler);
 
-// // Create function to display data  to the page
-// var displayData = function(data) {
-//     const { icon, description } = data.current.weather[0];
-//     const { temp, humidity, uvi, wind_speed } = data.current;
-//     console.log(icon, description, temp, humidity, wind_speed);
-    
 
-
-//     cityContainer.innerHTML = 
-//         `<div class="weatherInfo">
-//             <div>Temp</div>
-//             <div>${temp}°F:</div>
-//         </div>
-//         <div class="weatherInfo">
-//             <div>Wind</div>
-//             <div>${speed} MPH:</div>
-//         </div>
-//         <div class="weatherInfo">
-//             <div>Humidity</div>
-//             <div>${humidity}%:</div>
-//         </div>
-//         <div class="weatherInfo">
-//             <div>UV Index:</div>
-//             <div>${uvi}</div>
-//         </div> `
-// }
 
 
 
